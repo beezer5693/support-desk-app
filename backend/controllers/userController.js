66861f83aa694f833cb25d99f92c699898ea9893
Brandon Bryan <bshwag@gmail.com>
2023-03-30 16:21:17 -0400

    Added async handling package

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,9 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc    Register a new user
 // @route   /api/users
 // @access  Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
 	const { name, email, password } = req.body;
 
 	if (!name || !email || !password) {
@@ -10,14 +12,14 @@ const registerUser = (req, res) => {
 	}
 
 	res.send('Register route');
-};
+});
 
 // @desc    Login a user
 // @route   /api/users/login
 // @access  Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
 	res.send('Login route');
-};
+});
 
 module.exports = {
 	registerUser,
