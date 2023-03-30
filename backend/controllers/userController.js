6bd7db6941d4ec48747cab27aace832b9188693a
Brandon Bryan <bshwag@gmail.com>
2023-03-30 16:14:10 -0400

    Added middleware for error handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,20 @@
+// @desc    Register a new user
+// @route   /api/users
+// @access  Public
 const registerUser = (req, res) => {
+	const { name, email, password } = req.body;
+
+	if (!name || !email || !password) {
+		res.status(400);
+		throw new Error('Please inlcude all fields.');
+	}
+
 	res.send('Register route');
 };
 
+// @desc    Login a user
+// @route   /api/users/login
+// @access  Public
 const loginUser = (req, res) => {
 	res.send('Login route');
 };
