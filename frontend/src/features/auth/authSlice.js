4897adba308ad0c1721a8c861794a426768d1ffe
Brandon Bryan <bshwag@gmail.com>
2023-03-31 13:10:00 -0400

    Added asyncThunk register function

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,12 +1,19 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
-	user: null,
+	user: 'brandon',
 	error: false,
 	isSuccess: false,
 	isLoading: false,
 	message: ''
 };
+
+export const register = createAsyncThunk(
+	'auth/register',
+	async (user, thunkAPI) => {
+		console.log(user);
+	}
+);
 
 export const authSlice = createSlice({
 	name: 'auth',
