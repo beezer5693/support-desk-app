b9b45d6528b6192eab76376e538122cda881b222
Brandon Bryan <bshwag@gmail.com>
2023-04-01 11:57:06 -0400

    Added login functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FaSignInAlt } from 'react-icons/fa';
 import { toast } from 'react-toastify';
 import { useSelector, useDispatch } from 'react-redux';
-import { login } from '../features/auth/authSlice';
+import { login, reset } from '../features/auth/authSlice';
 
 function Login() {
 	const [formData, setFormData] = useState({
@@ -14,9 +15,24 @@ function Login() {
 
 	const dispatch = useDispatch();
 
-	const { user, isLoading, isSuccess, message } = useSelector(
+	const navigate = useNavigate();
+
+	const { user, isLoading, isError, isSuccess, message } = useSelector(
 		state => state.auth
 	);
+
+	useEffect(() => {
+		if (isError) {
+			toast.error(message);
+		}
+
+		// Redirect when logged in
+		if (isSuccess && user) {
+			navigate('/');
+		}
+
+		dispatch(reset);
+	}, [isError, isSuccess, user, message, navigate, dispatch]);
 
 	const onChange = e => {
 		setFormData(prevState => ({
@@ -40,7 +56,7 @@ function Login() {
 		<>
 			<section className='heading'>
 				<h1>
-					<FaSignInAlt /> Login {user}
+					<FaSignInAlt /> Login
 				</h1>
 				<p>Please Login To Get Support</p>
 			</section>
