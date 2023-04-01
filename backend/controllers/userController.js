798ce52767ef369af8f2b263a7702bf4ee9131c0
Brandon Bryan <bshwag@gmail.com>
2023-04-01 12:09:17 -0400

    Updated error text

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -64,7 +64,7 @@ const loginUser = asyncHandler(async (req, res) => {
 		});
 	} else {
 		res.status(401);
-		throw new Error('Invalid email or password');
+		throw new Error('Invalid credentials');
 	}
 });
 
